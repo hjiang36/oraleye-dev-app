e383bdabe679dcc7371b22db1003e5dc62ff9a65
Haomiao <haomiaojiang@roblox.com>
2024-06-28 20:23:32 -0700

    update decoder script

--- scripts/raw_decoder.py
+++ scripts/raw_decoder.py
@@ -1,25 +1,9 @@
 import os
-import struct
 import argparse
-import re
 import numpy as np
 import imageio
 
-"""
-Get width / height / exposure time from the filename.
-"""
-def extract_info(filename):
-    # Regular expression to find width (w\d+), height (h\d+), and exposure time (Expt_\d+us)
-    pattern = r'w(\d+)_h(\d+)_.*Expt_(\d+)us'
-    match = re.search(pattern, filename)
 
-    if match:
-        width = int(match.group(1))
-        height = int(match.group(2))
-        exposure_time = int(match.group(3))  # in microseconds
-        return width, height, exposure_time
-    else:
-        return None
 
 """
 Parse the raw binary file and convert it to a readable 16-bit PNG format.
@@ -33,7 +17,7 @@ def parse_args():
         '--normalize',
         type=bool,
         default=False,
-        help="Whether to normalize 12bit to fit full 16 bit")
+        help="Whether to normalize 10bit to fit full 16 bit")
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -44,31 +28,30 @@ if __name__ == '__main__':
     if not os.path.exists(args.input):
         raise ValueError("Input file does not exist")
     
-    # Parse filename to get width, height, and exposure time
-    width, height, exposure_time = extract_info(args.input)
-    # Create a numpy array
+    # Width, height, bit depth
+    width, height = 4608, 2592
+    bit_depth = 10
+    
+    # Read packed data
+    raw_data = np.fromfile(args.input, dtype=np.uint8)
+    if len(raw_data) != width * height * bit_depth // 8:
+        raise ValueError("Invalid file size")
+    
+    print(np.max(raw_data))
+    
+    # Unpack data
+    raw_data = np.reshape(raw_data, (height, width * bit_depth // 8)).astype(np.uint16)
     raw = np.zeros((height, width), dtype=np.uint16)
-    print("Width: {}, Height: {}, Exposure time: {} us".format(width, height, exposure_time))
 
-    # Read binary file
-    with open(args.input, 'rb') as f:
-        for i in range(height):
-            for j in range(0, width, 2):
-                # 12 bit per pixel
-                data = f.read(3)
-                if (len(data) != 3):
-                    print("Warning: Reached end of file")
-                    print(len(data))
-                    break
-                compressed = struct.unpack('BBB', bytearray(data))
-                raw[i, j] = compressed[0] | ((compressed[1] & 0x0F) << 8)
-                raw[i, j+1] = (compressed[2] << 4) | (compressed[1] >> 4)
+    for i in range(4):
+        raw[:, i::4] = raw_data[:, i::5] * 4 + ((raw_data[:, 4::5] >> (6 - 2 * i)) & 3)
+    
+    if args.normalize:
+        raw = raw * (2 ** 6)
+
+    print(np.min(raw), np.max(raw))
         
-        # Normalize to 16 bit if needed
-        if args.normalize:
-            raw = raw * 16
-        
-        # Write to PNG file
-        imageio.imwrite(args.output, raw)
+    # Write to PNG file
+    imageio.imwrite(args.output, raw)
 
     
